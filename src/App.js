0ca5a1e3b2abf46cc1f654d5ea95ce04eee7cef2
danycambrano <danycambrano1@gmail.com>
2022-04-27 12:43:21 -0500

    Se agregó el componente barraMenu.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import BarraMenu from './componentes/barraMenui';
 
 function App() {
 
-/*const mensaje=()=>{
+const mensaje=()=>{
   console.log('Hola a todos');
-}*/
+}
 
   return (
     <div>
       <h1>Hola a todos</h1>
-      <button onClick={()=>console.log('Hola a todos')}>Enviar</button>
+      <button onClick={mensaje}>Enviar</button>
+      <BarraMenu/>
     </div>
   );
 }
---
+++ src/componentes/barraMenui.js
@@ -0,0 +1,9 @@
+function BarraMenu(){
+    return(
+        <div>
+            <h1>Esta es la barra de navegación</h1>
+        </div>
+    )
+}
+
+export default BarraMenu;
