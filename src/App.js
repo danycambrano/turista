0051585b8d8626fb27d35c8b531f22a25b390db2
danycambrano <danycambrano1@gmail.com>
2022-05-02 11:29:16 -0500

    Agregamos una imagen en la seccion principal

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import BarraMenu from './componentes/barraMenui';
+import "./App.css";
+import BarraMenu from "./componentes/barraMenui";
+import Ropa from "./componentes/Ropa";
 
 function App() {
-
-const mensaje=()=>{
-  console.log('Hola a todos');
-}
+  const mensaje = () => {
+    console.log("Hola a todos");
+  };
 
   return (
-    <div>
-      <h1>Hola a todos</h1>
-      <button onClick={mensaje}>Enviar</button>
-      <BarraMenu/>
+    <div className="container">
+      <section>
+        <BarraMenu />
+      </section>
+      <section>
+        <Ropa />
+      </section>
     </div>
   );
 }
